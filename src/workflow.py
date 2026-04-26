561082541537b1115084ccca423626232e2954af
Sarahkh4 <sarahkhan8040@gmail.com>
2026-04-26 23:38:09 +0500

    workflow added

--- src/workflow.py
+++ src/workflow.py
@@ -7,13 +7,20 @@ from .nodes import(
 
 from langgraph.graph import START,END, STATEGRAPH
 
+def workflow_builder():
+    builder = StateGraph(State)
 
-builder = StateGraph(State)
+    builder.add_node("orchestrator",orchestrator)
+    builder.add_node("llm_call",llm_call)
+    builder.add_node("synthesizer",synthesizer)
+    builder.add_node("assign_workers",assign_workers)
 
-builder.add_node("orchestrator",orchestrator)
-builder.add_node("llm_call",llm_call)
-builder.add_node("synthesizer",synthesizer)
-builder.add_node("assign_workers",assign_workers)
+    builder.add_edge(START, "orchestrator")
+    builder.add_conditional_edge(
+        "orchestrator",
+        assign_workers , ["llm_call"]
+        )
+    builder.add_edge("llm_call", "synthesizer")
+    builder.add_edge("synthesizer", END)
 
-builder.add_edge()
-
+    return builder.compile()
